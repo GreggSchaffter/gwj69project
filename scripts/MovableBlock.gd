907f7ea3045c71217d0f9fea0325798638ee6175
aNamee <brendenmckinley06@gmail.com>
2024-05-13 13:26:08 -0800

    Made the boxes collide with other boxes

--- scripts/MovableBlock.gd
+++ scripts/MovableBlock.gd
@@ -1,18 +1,22 @@
 extends Node3D
 
 @onready var remote_transform = $RemoteTransform3D
+@onready var box_cast = $AnimatableBody3D/BoxCast
+
 @onready var plus_x = $"AnimatableBody3D/+X"
 @onready var minus_x = $"AnimatableBody3D/-X"
 @onready var plus_z = $"AnimatableBody3D/+Z"
 @onready var minus_z = $"AnimatableBody3D/-Z"
 
 
-const STEP_SIZE = 2;
+const STEP_SIZE = 1;
 const SPEED = 0.5;
 
 
-
 @export var moving = false
+
+func _ready():
+	force_grid()
 
 func _process(_delta):
 	if moving:
@@ -28,6 +32,12 @@ func _process(_delta):
 		move(Vector3.BACK)
 
 func move(dir):
+	# Use a shapecast in order to make sure that the box is not moving into another box
+	box_cast.target_position = dir * STEP_SIZE
+	box_cast.force_shapecast_update()
+	if will_smash_box():
+		return
+	
 	var movement_tween = get_tree().create_tween()
 	var going_to = remote_transform.position + dir * STEP_SIZE
 	movement_tween.tween_property(remote_transform, "position", going_to, SPEED)
@@ -36,9 +46,22 @@ func move(dir):
 
 func done_moving():
 	moving = false
+	force_grid()
 
 func has_player(object):
 	for i in object.get_overlapping_bodies():
 		if i.is_in_group("player"):
 			return true
 	return false
+
+func will_smash_box():
+	var amount_of_collions = box_cast.get_collision_count()
+	for id in amount_of_collions:
+		var collider  = box_cast.get_collider(id)
+		#collider.hide()
+		if collider.is_in_group("Movable Block"):
+			return true
+		return false
+
+func force_grid():
+	position = round(position / STEP_SIZE) * STEP_SIZE
