e7b040738a57d102929ad8abea0a69f6bc1b58db
aNamee <brendenmckinley06@gmail.com>
2024-05-13 09:53:39 -0800

    Improved detection method.

--- scripts/MovableBlock.gd
+++ scripts/MovableBlock.gd
@@ -1,10 +1,25 @@
 extends Node3D
 
 @onready var remote_transform = $RemoteTransform3D
+@onready var plus_x = $"AnimatableBody3D/+X"
+@onready var minus_x = $"AnimatableBody3D/-X"
+@onready var plus_z = $"AnimatableBody3D/+Z"
+@onready var minus_z = $"AnimatableBody3D/-Z"
+
 
 const STEP_SIZE = 2;
 
 @export var moving = false
+
+func _process(_delta):
+	if has_player(plus_x):
+		move(Vector3.RIGHT)
+	elif has_player(minus_x):
+		move(Vector3.LEFT)
+	elif has_player(plus_z):
+		move(Vector3.FORWARD)
+	elif has_player(minus_z):
+		move(Vector3.BACK)
 
 func move(dir):
 	if moving:
@@ -19,24 +34,8 @@ func move(dir):
 func done_moving():
 	moving = false
 
-func _plus_x(body):
-	if !body.is_in_group("player"):
-		return
-	move(Vector3.RIGHT)
-
-func _minus_x(body):
-	if !body.is_in_group("player"):
-		return
-	move(Vector3.LEFT)
-
-
-func _plus_z(body):
-	if !body.is_in_group("player"):
-		return
-	move(Vector3.FORWARD)
-
-
-func _minus_z(body):
-	if !body.is_in_group("player"):
-		return
-	move(Vector3.BACK)
+func has_player(object):
+	for i in object.get_overlapping_bodies():
+		if i.is_in_group("player"):
+			return true
+	return false
